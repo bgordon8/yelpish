391c163d6f30c9c3009e5f4e0782ee8d1ca714be
bgordon8 <gordonbrian16@gmail.com>
2021-05-04 16:07:11 -0600

    Adds new Campground through route for quick test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelpish", {
   useNewUrlParser: true,
@@ -21,6 +22,14 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "Cheap camping",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
