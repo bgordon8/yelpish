967a66f5f51b9f15efc2d6ebd921870631b8adaf
bgordon8 <gordonbrian16@gmail.com>
2021-05-16 11:42:13 -0600

    Adds public directory

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(methodOverride("_method"));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
