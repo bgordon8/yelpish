f3fcc6f664feab83a3153290b5fcb1bf0de2cdfd
bgordon8 <gordonbrian16@gmail.com>
2021-05-18 13:26:39 -0600

    Adds starability stylesheet

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const reviews = require("../controllers/reviews");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
-
 const catchAsync = require("../utils/catchAsync");
 
 router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
