d087750a25aa7d8536e478a3eb4056cafd00d3d0
bgordon8 <gordonbrian16@gmail.com>
2021-05-25 10:27:40 -0600

    Adds .env directory gitignore directory cloudinary APIKEY

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.MODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
